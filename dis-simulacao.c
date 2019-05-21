857362dda49c305ca159ddb86f86f048d142a4fd
Eber Martins Rodrigues <eber.rodrigues@neogrid.com>
2019-05-21 09:14:07 -0300

    Comentários nas structs

--- dis-simulacao.c
+++ dis-simulacao.c
@@ -2,20 +2,20 @@
 #define MAXNOME 100
 
 struct Banco {
-    int idBanco;
-    char nome[MAXNOME];
+    int idBanco; // identificador da instituição financeira
+    char nome[MAXNOME]; // nome da instituição financeira
 };
 
 struct Produto {
-    int idProduto;
-    int idBanco;
-    char nomeProduto[MAXNOME];
-    int disponivel;
-    char sistAmortizacao;
-    float maxPorcentFinanc;
-    float taxaEfetivaJuros;
-    int prazoMax;
-    float maxPorcentRenda;
+    int idProduto; // identificador do produto
+    int idBanco; // identificador da instituição financeira à qual o produto pertence (chave externa)
+    char nomeProduto[MAXNOME]; // nome do produto
+    int disponivel; // flag indicando se o produto está disponível ou não
+    char sistAmortizacao; // caracter indicando o sistema de amortização que pode ser SAC ('S') ou PRICE ('P')
+    float maxPorcentFinanc; // número entre 0 e 1 indicando a máxima porção financiável de um valor
+    float taxaEfetivaJuros; // número entre 0 e 1 indicando a taxa efetiva de juros
+    int prazoMax; // número inteiro indicando a máxima quantidade de meses permitida para o financiamento
+    float maxPorcentRenda; // número entre 0 e 1 indicando o máximo comprometimento da renda permitido
 };
 
 int main() {
